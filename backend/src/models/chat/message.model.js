cfb771df998810c7ce2915af706986bf72587db8
Ajay Barman <barmanjiaj@gmail.com>
2025-04-04 15:39:31 +0530

    feat(messages): add message schema/model

---
+++ backend/src/models/chat/message.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const messageSchema = new Schema(
+    {
+        chatId: {
+            type: Schema.Types.ObjectId,
+            ref: "Chat",
+            required: true,
+        },
+        senderId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        messageType: {
+            type: String,
+            required: true,
+        },
+        delivered: {
+            type: Boolean,
+            default: false,
+        },
+        seen: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+messageSchema.plugin(mongooseAggregatePaginate);
+export const Messages = mongoose.model("Messages", messageSchema);
